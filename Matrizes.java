0cb56a67aec51d36eca033fc5bdcaa2ac50158ad
LisbetBonilla <bonillaortiz.lisbeth@tiernoparla.es>
2022-10-18 19:40:32 +0200

    matriz NxN

--- Matrizes.java
+++ Matrizes.java
@@ -6,8 +6,10 @@ package com.tiernoparla.ejercicioelementales;
  import java.util.Random;
 
 public class Matrizes {
+    
     public static void main(String[] arg) {
 
+        //metodo que le das un N, y dibuja por pantalla una matriz NxN
         /*
         int N = 5;
         
@@ -29,18 +31,41 @@ public class Matrizes {
           for (int[] arr: matrix){
               System.out.println(Arrays.toString(arr));
           }
-          */
+         
+        
+         //matriz NxN de 0's y 1's aleatorios
+            final int N = 5;
+        
+           int[][] matrix = new int [N][N];
         
         Random rnd = new Random();
-        for(int i =0; i < 10; i++){
-            if(rnd.nextBoolean()){
-                System.out.println(1);
-            }else {
-                System.out.println(0);
-            }
-        }
+        for(int i =0; i < matrix.length; i++){
+           for(int j = 0; j < matrix[i].length; j++){
+              matrix[i][j] = (rnd.nextBoolean()) ? 1 : 0;
+            }//for j
+        }//for i
+           
+        for(int[] arr: matrix){
+            System.out.println(Arrays.toString(arr));
+        }//for
+        */
+        /*
+        for(int i = 0; i < 10; i++){
+            Random rnd2 = new Random();
+            System.out.println(rnd2.nextDouble());
+         }//for
+        */
         
+        final int N = 5;
         
+           int[][] matrix = new int [N][N];
         
+        Random rnd = new Random();
+        
+            for(int i =0; i < matrix.length; i++){
+                for (int j =0; j < matrix[i].length; j++){
+                 System.out.println((int)(Math.random()*2));
+                }//for j
+            }//for i
     }//main
 }//public class
